2c01c05495f2f43d3838e4bee42cefb084b79069
Dhruv Jain <dhruv@Dhruvs-MacBook-Pro.local>
2020-11-21 21:10:28 +0400

    why was rungvim still here?

--- vim/runvim.sh
+++ vim/runvim.sh
@@ -1,4 +1,5 @@
 ln -s ~/dotfiles/vim/.vimrc ~/
 ln -s ~/dotfiles/vim/.vim ~/
+ln -s ~/dotfiles/vim/.gvimrc ~/
 
 # git clone https://github.com/vim-airline/vim-airline ~/.vim/pack/dist/start/vim-airline
